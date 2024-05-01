95a102bc56bdb0ace47da6393741ec927efa70c7
enjoy61 <enjoy61@protonmail.com>
2024-05-01 11:10:10 +0800

    proj inited.

---
+++ Source/UnrealEngineNotes/UnrealEngineNotes.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealEngineNotes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEngineNotes, "UnrealEngineNotes" );
